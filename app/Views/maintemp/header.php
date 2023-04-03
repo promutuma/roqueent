0ef16e6d76937a5d41d0876a10744fe2815d2f52
Eng. F.Mutuma <admin@camera20production.co.ke>
2023-04-03 17:15:34 +0300

    header

--- app/Views/maintemp/header.php
+++ app/Views/maintemp/header.php
@@ -8,12 +8,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="A powerful and conceptual apps base dashboard template that especially build for developers and programmers.">
     <!-- Fav Icon  -->
-    <link rel="shortcut icon" href="<?php echo base_url()?>/files/images/favicon.png">
+    <link rel="shortcut icon" href="<?php echo base_url()?>files/images/favicon.png">
     <!-- Page Title  -->
     <title>Dashboard | DashLite Admin Template</title>
     <!-- StyleSheets  -->
-    <link rel="stylesheet" href="<?php echo base_url()?>/files/assets/css/dashlite.css?ver=2.9.1">
-    <link id="skin-default" rel="stylesheet" href="<?php echo base_url()?>/files/assets/css/theme.css?ver=2.9.1">
+    <link rel="stylesheet" href="<?php echo base_url()?>files/assets/css/dashlite.css?ver=2.9.1">
+    <link id="skin-default" rel="stylesheet" href="<?php echo base_url()?>files/assets/css/theme.css?ver=2.9.1">
 
     <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM=" crossorigin="anonymous"></script>
 
