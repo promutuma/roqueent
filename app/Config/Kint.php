996484c8982de237d82ea633945d0e4ec16b8e23
Mutuma <framutuma@gmail.com>
2026-04-01 21:15:28 +0300

    chore: update Kint sort order and register Config namespace in composer.json

--- app/Config/Kint.php
+++ app/Config/Kint.php
@@ -41,7 +41,7 @@ class Kint
     */
     public string $richTheme = 'aante-light.css';
     public bool $richFolder  = false;
-    public int $richSort     = AbstractRenderer::SORT_FULL;
+    public int $richSort     = AbstractRenderer::SORT_ALPHABETICAL;
 
     /**
      * @var array<string, class-string<ValuePluginInterface>>|null
